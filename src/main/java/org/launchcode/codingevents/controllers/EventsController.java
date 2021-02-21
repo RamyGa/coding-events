e7d2e90d25af0bfad616317e328f41f91313ac76
Ramyga <research@ronolabs.com>
2021-02-21 17:32:22 -0600

    Event handlers added, htmls pages added

---
+++ src/main/java/org/launchcode/codingevents/controllers/EventsController.java
@@ -0,0 +1,48 @@
+package org.launchcode.codingevents.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("events")
+public class EventsController {
+
+    @GetMapping
+    public String displayAllEvents(Model model){
+        List<String> events = new ArrayList<>();
+        events.add("code with pride");
+        events.add("Strange loop");
+        events.add("Apple WWDC");
+        events.add("SpringOne Platform");
+
+        model.addAttribute("events", events);
+
+        return "events/index";
+
+    }
+
+
+    // lives at /events/create
+    @GetMapping("create")
+    public String renderCreateEventForm(){
+
+        return "events/create";
+
+    }
+
+    @PostMapping("create")
+    public String renderCreateEventForm2(){
+
+        return "index";
+
+    }
+
+
+}
---
+++ src/main/java/org/launchcode/codingevents/controllers/HomeController.java
@@ -0,0 +1,17 @@
+package org.launchcode.codingevents.controllers;
+
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HomeController {
+
+    public String getIndex(){
+
+
+     return "index";
+    }
+
+
+
+}
